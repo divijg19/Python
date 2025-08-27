c4ea715de6048e07956f755f8fae987f0865128f
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-08-28 04:25:48 +0530

    Implement Repository class with initialization and string methods

--- month1_python_mastery/week1_core_python/week1_task.py
+++ month1_python_mastery/week1_core_python/week1_task.py
@@ -1,2 +1,10 @@
 class Repository:
-  pass
+  def __init__ (self, name, language, stars):
+    print(f"Creating a new object repository for '{name}'...")
+    self.name = name
+    self.language = language
+    self.stars = stars
+  def __str__(self):
+    return f"{self.name} is a {self.language} repository with {self.stars}"
+  def __repr__(self):
+    return f"Repository({name} is '{self.name}', {language} is '{self.language} and {stars} is {self.stars})"
